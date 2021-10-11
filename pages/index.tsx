6a143845826fc98a42ab68bc3b1d4f2a08f4261c
Tomáš Kalina <info@tomaskalina.cz>
2021-10-11 13:18:27 +0200

    Implement `next-with-apoll`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,20 @@
+import { getDataFromTree } from "@apollo/client/react/ssr";
+import React from "react";
 import App from "../components/App";
-import PostList, { ALL_POSTS_QUERY } from "../components/PostList";
-import { initializeApollo, addApolloState } from "../lib/apolloClient";
+import PostList from "../components/PostList";
+import withApollo from "../lib/withApollo";
 
-const IndexPage = () => (
-  <App>
-    <PostList />
-  </App>
-);
+const Index = () => {
+  return (
+    <App>
+      <PostList />
+    </App>
+  );
+};
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo();
+// export default withApollo(Index);
 
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-  });
-
-  return addApolloState(apolloClient, {
-    props: {},
-    revalidate: 1,
-  });
-}
-
-export default IndexPage;
+// You can also override the configs for withApollo here, so if you want
+// this page to have SSR (and to be a lambda) for SEO purposes and remove
+// the loading state, uncomment the import at the beginning and this:
+export default withApollo(Index, { getDataFromTree });
