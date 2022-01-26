f0eee2e68939ca10e996ddb1de1c7585f659781a
Tomáš Kalina <info@tomaskalina.cz>
2022-01-26 08:50:02 +0100

    Implement spinner

--- pages/page/[id].tsx
+++ pages/page/[id].tsx
@@ -2,20 +2,35 @@ import { useRouter } from "next/router";
 import * as React from "react";
 import Layout from "../../components/Layout";
 import { useDetailPageQuery } from "../../_codegen";
-
+import Error from "../../components/Error";
+import LoadingPage from "../../components/LoadingPage";
 interface Props {}
 
 const Page: React.FC<Props> = ({}) => {
   const router = useRouter();
 
-  const data = useDetailPageQuery({
+  const { loading, error, data } = useDetailPageQuery({
     variables: { id: String(router.query.id) },
   });
+
+  if (error)
+    return (
+      <Layout>
+        <Error data={error} />
+      </Layout>
+    );
+  if (loading)
+    return (
+      <Layout>
+        <LoadingPage />
+      </Layout>
+    );
+
   return (
     <Layout>
       <div
         dangerouslySetInnerHTML={{
-          __html: data.data?.page?.content ?? "",
+          __html: data?.page?.content ?? "",
         }}
       />
     </Layout>
