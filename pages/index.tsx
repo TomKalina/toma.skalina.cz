e8a66c2e1e65ca68a622c5131d3608e4799095f6
Tomáš Kalina <info@tomaskalina.cz>
2022-01-26 11:25:41 +0100

    Fix link

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Index = () => {
       </h2>
       <div className="flex items-center justify-center mt-4">
         <a
-          href="mail:info@tomaskalina.cz"
+          href="mailto:info@tomaskalina.cz"
           className="uppercase py-2 my-2 px-4 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"
         >
           CONNECT WITH ME
