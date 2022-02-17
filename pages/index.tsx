8405c7ab1930a2129579324a62d533aa506e77f8
Tomáš Kalina <info@tomaskalina.cz>
2022-02-17 18:51:34 +0100

    Remove connect

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,6 @@ const Index = () => {
       <p className="text-3xl my-6 text-center dark:text-white">
         Hi, I&#x27;m Tom 🤘
       </p>
-      <div className="flex items-center justify-center mt-4">
-        <a
-          href="mailto:info@tomaskalina.cz"
-          className="uppercase py-2 my-2 px-4 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"
-        >
-          CONNECT WITH ME
-        </a>
-      </div>
     </Layout>
   );
 };
